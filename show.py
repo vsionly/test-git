d9b0bf114846bc67fdb3da88ccc0b884d24ed880
vsionly <liweisheng@kangfuzi.cn>
2017-11-02 20:06:05 +0800

    study python

---
+++ show.py
@@ -0,0 +1,13 @@
+# name = input('请输入你的名字：')
+# print('he"llo" ',name)
+# -*- coding: UTF-8 -*-
+L = ['Bart', 'Lisa', 'Adam']
+
+# while :
+#     pass
+l=len(L)
+
+while l>0:
+    pass
+    l = l-1
+    print(L[l])
